83282721ea8880f4d3208b853e0b4f92ac57b7e4
mckim <kmc04@naver.com>
2021-12-28 18:22:20 +0900

    기본 UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 const Home: NextPage = () => {
-  return <div className="text-2xl text-red-200">hello</div>
+  return <div className="h-screen w-screen flex flex-col justify-center items-center">
+    <div className="text-center">당신의 포켓몬은?</div>
+    <div className="p-2"></div>
+    <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+      <div className="w-16 h-16 bg-red-200"></div>
+      <div className="p-8">VS</div>
+      <div className="w-16 h-16 bg-red-200"></div>
+    </div>
+  </div>
 }
 
 export default Home
